925fa5e6eae988bbb6417b2a72ce29521c04c155
Nachiketa Vadera <personal.nachiketa@gmail.com>
2019-01-01 12:54:30 +0530

    Changed EditText to material

--- app/src/main/java/android/nachiketa/ebookdownloader/Global.java
+++ app/src/main/java/android/nachiketa/ebookdownloader/Global.java
@@ -5,21 +5,6 @@ import android.vadera.nachiketa.pen_paper.AndroidReadWrite;
 import java.util.Random;
 
 class Global {
-
-//    String getRandomQuote() throws IOException {
-//        Random random = new Random();
-//        StringBuilder builder = new StringBuilder();
-//        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("res/raw/quotes.txt");
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-//        String line;
-//        while ((line = bufferedReader.readLine()) != null) {
-//            builder.append(line);
-//        }
-//        int randomNumber = random.nextInt(37);
-//        String[] temp = builder.toString().split("~");
-//        return temp[randomNumber];
-//    }
-
     String getRandomQuote() {
         String[] quotes = new AndroidReadWrite().loadFromRaw("quotes.txt").split("~");
         return quotes[new Random().nextInt(37)];
